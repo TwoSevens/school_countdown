6f325e746ea6306fc801d8bd2a5912c620e31482
Mohammed Adel <mohammed28adel@gmail.com>
2024-08-01 01:39:42 +0300

    Initial

---
+++ src/App.tsx
@@ -0,0 +1,49 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+import moment from "moment";
+
+function App() {
+  let schoolStart = new Date(2024, 8, 5, 7, 30).getTime();
+  let currentTime = Date.now();
+  let [countdown, setCountdown] = useState(
+    moment.duration(schoolStart - currentTime)
+  );
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const currentTime = Date.now();
+      setCountdown(moment.duration(schoolStart - currentTime));
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  if (currentTime < schoolStart) {
+    return (
+      <>
+        <div>
+          <h1>
+            {countdown.months()} Month{countdown.months() > 1 && "s"},{" "}
+            {countdown.days()} Day{countdown.days() > 1 && "s"},{" "}
+            {countdown.hours()} Hour{countdown.hours() > 1 && "s"},{" "}
+            {countdown.minutes()} Minute{countdown.minutes() > 1 && "s"}, and{" "}
+            {countdown.seconds()} Second{countdown.seconds() > 1 && "s"} until
+            school.
+          </h1>
+
+          <h1>Go to school! It already started.</h1>
+        </div>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <div>
+          <h1>Go to school! It already started.</h1>
+        </div>
+      </>
+    );
+  }
+}
+
+export default App;
